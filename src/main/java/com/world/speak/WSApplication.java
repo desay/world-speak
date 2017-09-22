056acdd38795337c60a256ba7853f75ca502bc09
Helga Pavluchenko <olga@pavluchenko.com>
2017-09-22 12:44:05 +0300

    add maven and base class

---
+++ src/main/java/com/world/speak/WSApplication.java
@@ -0,0 +1,34 @@
+package com.world.speak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by Helga on 22.09.17.
+ */
+
+@Configuration
+@Service
+@Transactional
+@SpringBootApplication
+@EnableJpaRepositories(
+        basePackages = {
+                "com.world.speak.repository"
+        }
+)
+@EntityScan(
+        basePackages = {
+                "com.world.speak.domain"
+        }
+)
+public class WSApplication {
+        public static void main(String[] args) throws Exception {
+                SpringApplication.run(WSApplication.class, args);
+        }
+
+}
